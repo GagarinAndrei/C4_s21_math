e867827d43a0491bc0530a69ef6373c588448db0
Dawn Kali <gagarin1982@gmail.com>
2023-12-06 16:55:16 +0300

    dawnkali: clang

--- src/lib/s21_floor.c
+++ src/lib/s21_floor.c
@@ -3,4 +3,4 @@
 long double s21_floor(double x) {
   if (x == -0.0) return x;
   return (x < 0 && x != (int)x) ? (int)x - 1 : (int)x;
-}  //готово
+}  // готово
--- src/tests/tests.c
+++ src/tests/tests.c
@@ -4,13 +4,10 @@
 
 int main(void) {
   int failed = 0;
-  Suite *s21_math[] = {test_abs(), test_acos(), test_asin(), test_atan(),
-                       test_ceil(), test_cos(), test_exp(), test_fabs(),
-                       test_floor(), test_fmod(),
-                       test_log(), 
-                       test_pow(), test_sin(),
-                       test_sqrt(), //в бесконечный цикл уходит
-                       test_tan(), NULL};
+  Suite *s21_math[] = {test_abs(),   test_acos(), test_asin(), test_atan(),
+                       test_ceil(),  test_cos(),  test_exp(),  test_fabs(),
+                       test_floor(), test_fmod(), test_log(),  test_pow(),
+                       test_sin(),   test_sqrt(), test_tan(),  NULL};
   for (int i = 0; s21_math[i] != NULL; i++) {
     SRunner *sr = srunner_create(s21_math[i]);
 
